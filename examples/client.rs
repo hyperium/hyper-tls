287d7ed942670ae0e8bbd63d5515f6ac937cc27f
Sean McArthur <sean@seanmonstar.com>
2018-03-18 09:08:12 -0700

    update to hyper 0.12.x

--- examples/client.rs
+++ examples/client.rs
@@ -1,26 +1,28 @@
 extern crate futures;
 extern crate hyper;
 extern crate hyper_tls;
-extern crate tokio_core;
+extern crate tokio;
 
-use futures::{Future, Stream};
+use futures::{future, Future, Stream};
 use std::io::Write;
 
 fn main() {
-    let mut core = tokio_core::reactor::Core::new().unwrap();
-    let handle = core.handle();
-    let client = hyper::Client::configure()
-        .connector(hyper_tls::HttpsConnector::new(4, &handle).unwrap())
-        .build(&handle);
+    tokio::run(future::lazy(|| {
+        let https = hyper_tls::HttpsConnector::new(4).unwrap();
+        let client = hyper::Client::builder()
+            .build::<_, hyper::Body>(https);
 
-    let work = client.get("https://hyper.rs".parse().unwrap()).and_then(|res| {
-        println!("Status: {}", res.status());
-        println!("Headers:\n{}", res.headers());
-        res.body().for_each(|chunk| {
-            ::std::io::stdout().write_all(&chunk)
-                .map(|_| ())
-                .map_err(From::from)
-        })
-    });
-    core.run(work).unwrap();
+        client
+            .get("https://hyper.rs".parse().unwrap())
+            .and_then(|res| {
+                println!("Status: {}", res.status());
+                println!("Headers:\n{:#?}", res.headers());
+                res.into_body().for_each(|chunk| {
+                    ::std::io::stdout()
+                        .write_all(&chunk)
+                        .map_err(|e| panic!("example expects stdout to work: {}", e))
+                })
+            })
+            .map_err(|e| println!("request error: {}", e))
+    }));
 }
