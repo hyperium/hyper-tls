3d1f412d171cd8d9a5efc9396c85ef084a9ef4bd
Sean McArthur <sean@seanmonstar.com>
2018-10-02 11:50:03 -0700

    v0.3.1

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@
 //!     }));
 //! }
 //! ```
-#![doc(html_root_url = "https://docs.rs/hyper-tls/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/hyper-tls/0.3.1")]
 #![cfg_attr(test, deny(warnings))]
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
