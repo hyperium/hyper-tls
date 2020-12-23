8d3a7bcdab06abcb240dedc5e77cbc78d757e600
Teo Klestrup Röijezon <teo@nullable.se>
2020-12-23 23:13:07 +0100

    Upgrade to Tokio 1.0
    Currently pulling in tokio-native-tls via a git dependency, since
    https://github.com/tokio-rs/tls/pull/46 hasn't been released to crates.io yet.

--- examples/client.rs
+++ examples/client.rs
@@ -1,9 +1,8 @@
-
-use hyper::{Client, body::HttpBody as _};
+use hyper::{body::HttpBody as _, Client};
 use hyper_tls::HttpsConnector;
 use tokio::io::{self, AsyncWriteExt as _};
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
@@ -15,9 +14,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     while let Some(chunk) = res.body_mut().data().await {
         let chunk = chunk?;
-        io::stdout()
-            .write_all(&chunk)
-            .await?
+        io::stdout().write_all(&chunk).await?
     }
     Ok(())
 }
